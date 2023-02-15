b25a1c695cc5d6f6e09b61b06d1d14f8b105591f
Patrick <Patrick@LAPTOP-B9118I17>
2023-02-15 15:30:11 -0700

    Services and db classes updated for all models. (possible bugs)

---
+++ src/java/models/Category.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ * Category model
+ * @author Patrick
+ */
+public class Category {
+    private String name;
+    private String desc;
+    private double rate;
+    private int min;
+    private int max;
+
+    public Category() {
+    }
+
+    public Category(String name, String desc, double rate, int min, int max) {
+        
+        this.name = name;
+        this.desc = desc;
+        this.rate = rate;
+        this.min = min;
+        this.max = max;
+    }
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+    
+    
+}
